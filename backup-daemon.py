c43145abf39c4ea3dee30549ecb49765b7b3c727
Will Essilfie <wke2102@columbia.edu>
2017-08-12 21:26:07 -0700

    remove old data files from repo

--- backup-daemon.py
+++ backup-daemon.py
@@ -3,6 +3,7 @@ import subprocess
 import time
 
 with daemon.DaemonContext():
+	#make backup file
     print "Creating backup at %s" % datetime.datetime.now()
     subprocess.call(['./backup.sh'])
     time.sleep(3600)
