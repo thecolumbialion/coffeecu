1adf7c32d23f7d48c3cf02274c1a69c618f95ba3
Will Essilfie <wke2102@columbia.edu>
2017-08-12 22:39:28 -0700

    fixing image upload

--- server/init.js
+++ server/init.js
@@ -9,13 +9,13 @@ Meteor.startup(function () {
   // For image uplods using meteor-uploads
   UploadServer.init({
     tmpDir: process.env.PWD + '/.uploads/tmp',
-    uploadDir: process.env.PWD + '/.uploads/',
+    uploadDir: process.env.PWD + '/upload',
     checkCreateDirectories: true, // create the directories if not present
     getFileName: function (fileInfo, formData) { // rename files to <id>_dp.jpg 
       return formData.id + '_dp.jpg';
     },
     finished: function (fileInfo, formFields) { // construct final url baseUrl/uploads/profilePic/<id>_dp.jpg
-      fileInfo.finalUrl = "/upload" + fileInfo.path;
+      fileInfo.finalUrl = "/upload/" + fileInfo.path;
     },
     maxFileSize: 10000000, // 10 MB 
     mimeTypes: {
