354947bbbede45224215d53d40a50316f269a4ad
Will Essilfie <wke2102@columbia.edu>
2017-08-26 10:07:30 -0400

    new logo added and updated site language

--- client/home/parallax.js
+++ client/home/parallax.js
@@ -6,6 +6,7 @@ Template.parallax.rendered = function () {
 
 Template.parallax.helpers({
   'randomParallaxImage': function () {
+    //var images = new Array('lowmorning', 'halalcart', 'grandcentral', 'subway', 'brooklynbridge', 'cityview','almainsnow', 'butler', 'butlerwithalma', 'engineer', 'lion', 'low', 'seas150', 'wallach', 'yule');
     var images = new Array('almainsnow', 'butler', 'butlerwithalma', 'engineer', 'lion', 'low', 'seas150', 'wallach', 'yule');
     var randNum = Math.floor(Math.random() * images.length);
     return images[randNum];
