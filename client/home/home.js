7b3ea0b5989f820ae4a2657f8a0001c8166fd300
howell.devon@gmail.com <howell.devon@gmail.com>
2018-03-30 13:24:58 -0400

    commiting changes to accordion

--- client/home/home.js
+++ client/home/home.js
@@ -8,12 +8,13 @@ Tracker.autorun(function () {
   Meteor.subscribe('people-master');
 });
 
+//"currently selected" tracts the last profile clicked
 Session.set('currentlySelected', null);
 
 Template.people.rendered = function () {
   $(document).ready(function(){
-    $('.ui.accordion').accordion({exclusive: true});
 
+    //load more profile when the user scrolls to the bottom
     window.onscroll = function(ev) { if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
       setTimeout(function(){
         $('.load-more-button').click();
@@ -29,6 +30,7 @@ Template.intro.helpers({
   }
 });
 
+
 Template.meetingsMade.rendered = function () {
   // Update #meetings every second
   Meteor.setInterval(function () {
@@ -39,6 +41,7 @@ Template.meetingsMade.rendered = function () {
 };
 
 Template.meetingsMade.helpers({
+  //returns number of mealitings made
   'meetings': function () {
     return Session.get('meetings');
   }
@@ -68,69 +71,48 @@ Template.people.helpers({
 
 
 Template.people.events({
+  //when a profile is clicked
   'click #contact': function () {
 
-    for (property in this){
-      console.log(property);
-    }
-    //Session.set('currentlySelected', { owner: this.owner, uni: this.uni, name: this.name.split(' ')[0], image: this.image, makeVisible:this.makeVisible });
     Session.set('currentlySelected', this);
-    //Template.uniPrompt.name = this.name.split(' ')[0]);
     
-  $('.coupled.modal')
-    .modal({
-      allowMultiple: false
-    })
-  ;
-  // attach events to buttons
-  $('.second.modal')
-    .modal({
-      onApprove: function(event) {
-        var receiver = Session.get('currentlySelected').owner;
-        var receiverUni = Session.get('currentlySelected').uni;
-        var receiverName = Session.get('currentlySelected').name;
-        var additionalMessage = $("#additionalMessage").val();
-        var recaptcha = reCAPTCHA.getResponse("1");
-        Meteor.call('processSendRequest', Meteor.userId(), receiver, receiverUni, receiverName, additionalMessage, recaptcha,function (error, response) {
-        if (error) {
-            Materialize.toast('Failed to send email', 4000);
-            console.log(error);
-          } else {
-            Materialize.toast(response, 4000);
-          }
-        });
-          
-        reCAPTCHA.reset("1");
-      }
-    }).modal('attach events', '.first.modal .button.primary');
-  // show first now
-  $('.first.modal').modal({name: Session.get('currentlySelected').name})
-    .modal('show')
-  ;
-/*
-    $('.ui.modal').modal({
-      onApprove: function(event) {
-        var receiver = Session.get('currentlySelected').owner;
-        var receiverUni = Session.get('currentlySelected').uni;
-        var receiverName = Session.get('currentlySelected').name;
-        var additionalMessage = $("#additionalMessage").val();
-        var recaptcha = reCAPTCHA.getResponse("1");
-        Meteor.call('processSendRequest', Meteor.userId(), receiver, receiverUni, receiverName, additionalMessage, recaptcha,function (error, response) {
-        if (error) {
-            Materialize.toast('Failed to send email', 4000);
-            console.log(error);
-          } else {
-            Materialize.toast(response, 4000);
-          }
-        });
-          
-        reCAPTCHA.reset("1");
-      }
-    }).modal('show');
-*/    
+    //show 2 modal
+    $('.coupled.modal')
+      .modal({
+        //make 2nd modal modal dependent on the first
+        allowMultiple: false
+      })
+    ;
+
+    // attach events to buttons
+    $('.second.modal')
+      .modal({
+        onApprove: function(event) {
+          var receiver = Session.get('currentlySelected').owner;
+          var receiverUni = Session.get('currentlySelected').uni;
+          var receiverName = Session.get('currentlySelected').name;
+          var additionalMessage = $("#additionalMessage").val();
+          var recaptcha = reCAPTCHA.getResponse("1");
+          Meteor.call('processSendRequest', Meteor.userId(), receiver, receiverUni, receiverName, additionalMessage, recaptcha,function (error, response) {
+          if (error) {
+              Materialize.toast('Failed to send email', 4000);
+              console.log(error);
+            } else {
+              Materialize.toast(response, 4000);
+            }
+          });
+            
+          reCAPTCHA.reset("1");
+        }//omly connect primary button or else canceling 1st modal, cancels all photos
+      }).modal('attach events', '.first.modal .button.primary');
+    // show first modal
+    $('.first.modal').modal({name: Session.get('currentlySelected').name})
+      .modal('show')
+    ;  
   }
 });
 
+//template for the modals
 Template.uniPrompt.helpers({
   getUserProperty(property){
     currentuser = Session.get("currentlySelected");
