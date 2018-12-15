25d14c5d6dc299b7f8fd3bbb40618f7c42b3e464
Sanford Miller <skm2159@columbia.edu>
2018-12-15 04:49:16 -0500

    removing deployment stuff from git

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SSH_TO="root@159.203.139.9"
-
-if ssh $SSH_TO "cp /opt/coffeecu/current/bundle/upload/* /opt/coffeecu-photos"
-then
-    mup deploy
-    ssh $SSH_TO "cp /opt/coffeecu/current/bundle/upload/* /opt/coffeecu-photos"
-fi
